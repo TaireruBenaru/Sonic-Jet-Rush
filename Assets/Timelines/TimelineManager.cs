1e1dd6751844e24accbda28bbd17f948b1d0280b
Taireru Benaru <38292283+TaireruBenaru@users.noreply.github.com>
2022-11-24 15:02:15 +0100

    Create Timeline

---
+++ Assets/Timelines/TimelineManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimelineManager : MonoBehaviour
+{
+    public IEnumerator PlayTimeline(CharacterFlag character, int zone)
+    {
+
+    }
+}
